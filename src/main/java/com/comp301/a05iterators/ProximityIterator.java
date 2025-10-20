667b344b482404532473ae35cee36c1b1679a3b7
anikash1232 <anirudh.vpk@gmail.com>
2025-10-19 23:35:23 -0400

    trying the first class

---
+++ src/main/java/com/comp301/a05iterators/ProximityIterator.java
@@ -0,0 +1,60 @@
+package com.comp301.a05iterators;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ProximityIterator implements Iterator<Driver> {
+    private Iterator<Driver> driverPool;
+    private Position clientPosition;
+    private int proximityRange;
+    private Driver nextDriver;
+
+    public ProximityIterator (Iterable<Driver> driverPool, Position clientPosition, int proximityRange){
+        this.driverPool = driverPool.iterator();
+        this.clientPosition = clientPosition;
+        this.proximityRange = proximityRange;
+
+        this.nextDriver = null;
+    }
+
+    private void loadNextDriver(){
+        if (nextDriver != null){
+            return;
+        }
+        else{
+            while(driverPool.hasNext()){
+                Driver driver = driverPool.next();
+                Position driverPos = driver.getVehicle().getPosition();
+                if (driverPos.getManhattanDistanceTo(clientPosition) <= proximityRange){
+                    nextDriver = driver;
+                    return;
+                }
+            }
+            nextDriver = null;
+        }
+    }
+    @Override
+    public boolean hasNext(){
+        loadNextDriver();
+        if (nextDriver != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public Driver next(){
+        if (hasNext() == false){
+            throw new NoSuchElementException();
+        }
+        else{
+            Driver current = nextDriver;
+            nextDriver = null;
+            return current;
+
+        }
+
+    }
+}
